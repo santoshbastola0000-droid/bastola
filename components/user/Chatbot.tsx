5ccbec3c1175da02f1b4749add038f07b1520994
santosh08888 <namaeman8@gmail.com>
2026-08-08 10:20:41 +0545

    Improve chatbot open close animation

--- components/user/Chatbot.tsx
+++ components/user/Chatbot.tsx
@@ -529,10 +529,13 @@ const deductBalanceForText = (text: string) => {
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.18 }}
+            initial={{ opacity: 0, scale: 0.96, y: 18 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.96, y: 18 }}
+            transition={{
+              duration: 0.22,
+              ease: [0.22, 1, 0.36, 1],
+            }}
             className="
               fixed
               inset-0
