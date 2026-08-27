49f74958f55653c2b3fd0d7c6ab215d932272eef
santoshbastola0000-droid <santoshbastola0000@gmail.com>
2026-08-27 11:23:19 +0545

    Load nearby rooms first for opted-in users (#24)
    * feat: retain latest viewer coordinates for nearby feed
    
    * feat: send viewer location when loading public rooms

--- components/AutoLocationUpdate.tsx
+++ components/AutoLocationUpdate.tsx
@@ -5,7 +5,7 @@ export function AutoLocationUpdate() {
   useEffect(() => {
     if (!('geolocation' in navigator)) return;
     navigator.geolocation.getCurrentPosition(
-      ({ coords }) => privateApi.post('/user/location', { latitude: coords.latitude, longitude: coords.longitude }).catch(() => undefined),
+      ({ coords }) => { localStorage.setItem('roomkhoj-viewer-location', JSON.stringify({ latitude: coords.latitude, longitude: coords.longitude })); privateApi.post('/user/location', { latitude: coords.latitude, longitude: coords.longitude }).catch(() => undefined); },
       () => undefined,
       { enableHighAccuracy: false, maximumAge: 24 * 60 * 60 * 1000, timeout: 10000 },
     );
