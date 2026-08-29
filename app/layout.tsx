2a1a587dfcfd38fd3b2f4e8a39494d79f06245b1
santoshbastola0000-droid <santoshbastola0000@gmail.com>
2026-08-29 22:53:02 +0545

    Sync browser permissions for signed-in users

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import { PwaInstallPrompt } from "@/components/PwaInstallPrompt";
 import { LanguageProvider } from "@/components/providers/LanguageProvider";
 import { AutoPushPermission } from "@/components/AutoPushPermission";
 import { AutoLocationUpdate } from "@/components/AutoLocationUpdate";
+import { PermissionStatusSync } from "@/components/PermissionStatusSync";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -107,6 +108,7 @@ export default function RootLayout({
         <SessionChecker />
         <AutoPushPermission />
         <AutoLocationUpdate />
+        <PermissionStatusSync />
         <QueryClientProviderWrapper>
           <TooltipProvider>
             <GuestLoginPopup />
