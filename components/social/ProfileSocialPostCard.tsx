6b0d4c59f2f56e67c63b7e0b73512dc1df949a66
santoshbastola0000-droid <santoshbastola0000@gmail.com>
2026-09-19 23:32:08 +0545

    Make social post hashtags clickable

--- components/social/ProfileSocialPostCard.tsx
+++ components/social/ProfileSocialPostCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { HashtagText } from "@/components/social/HashtagText";
 import Link from "next/link";
 import { useRef, useState } from "react";
 import { MoreHorizontal } from "lucide-react";
@@ -322,7 +323,7 @@ export function ProfileSocialPostCard({
 
       {post.content && (
         <p className="whitespace-pre-wrap px-3 pb-3 text-[16px] font-normal leading-[1.35] text-slate-950">
-          {post.content}
+          <HashtagText text={post.content} />
         </p>
       )}
 
